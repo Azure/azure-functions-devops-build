a107652d77d08666d38fbf82a3d60c6ecc9db496
dolko <oliver.dolk@live.com.au>
2019-01-29 13:37:09 -0800

    adding package data

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@
 from setuptools import setup, find_packages
 
 NAME = "azure-functions-devops-build"
-VERSION = "0.0.1"
+VERSION = "0.0.3"
 
 # To install the library, run the following
 #
@@ -29,7 +29,7 @@ setup(
     keywords=["Microsoft", "Azure Devops", "Azure Functions"],
     install_requires=REQUIRES,
     packages=find_packages(),
-    include_package_data=True,
+    package_data={'azure_functions_devops_build' : ['*.jinja']},
     long_description="""\
     """
 )
