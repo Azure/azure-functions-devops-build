5dacd121c131e65e92880f1f75d43b20fdc18f90
dolko <oliver.dolk@live.com.au>
2019-02-05 16:13:19 -0800

    excluding tests from pypi package as it includes extra dependencies

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@
 from setuptools import setup, find_packages
 
 NAME = "azure-functions-devops-build"
-VERSION = "0.0.6"
+VERSION = "0.0.8"
 
 # To install the library, run the following
 #
@@ -25,10 +25,10 @@ setup(
     version=VERSION,
     description="Python package for integrating azure functions with azure devops. Specifically made for the Azure Cli",
     author_email="t-oldolk@microsoft.com",
-    url="https://github.com/dolko/azure-devops-build-manager",
+    url="https://github.com/Azure/azure-functions-devops-build",
     keywords=["Microsoft", "Azure Devops", "Azure Functions"],
     install_requires=REQUIRES,
-    packages=find_packages(),
+    packages=find_packages(exclude=["tests.*", "tests"]),
     include_package_data=True,
     long_description="""\
     """
