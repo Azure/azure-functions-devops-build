d2d8e50abaa181a70359e0f3f1161195e6e3822d
Hanzhang Zeng (Roger) <hazeng@microsoft.com>
2019-04-10 18:55:52 -0700

    Update: lincense in setup.py

--- setup.py
+++ setup.py
@@ -4,25 +4,25 @@
 # license information.
 #--------------------------------------------------------------------------
 
+from os import path
 from setuptools import setup, find_packages
 
 NAME = "azure-functions-devops-build"
-VERSION = "0.0.16"
+VERSION = "0.0.17"
 
-# To install the library, run the following
-#
-# python setup.py install
-#
-# prerequisite: setuptools
-# http://pypi.python.org/pypi/setuptools
+REQUIRES = ["msrest>=0.6.4",
+            "vsts>=0.1.25",
+            "Jinja2>=2.10"]
 
-REQUIRES = ["msrest",
-            "vsts",
-            "jinja2"]
+file_directory = path.abspath(path.dirname(__file__))
+
+with open(path.join(file_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name=NAME,
     version=VERSION,
+    license="MIT",
     description="Python package for integrating Azure Functions with Azure DevOps. Specifically made for the Azure CLI",
     author="Oliver Dolk, Hanzhang Zeng",
     author_email="t-oldolk@microsoft.com, hazeng@microsoft.com",
@@ -31,5 +31,6 @@ setup(
     install_requires=REQUIRES,
     packages=find_packages(exclude=["tests", "sample_yaml_builds"]),
     include_package_data=True,
-    long_description=""
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
