92aab95178f416809be7441ffe43b0d81d21e52d
Hanzhang Zeng (Roger) <hazeng@microsoft.com>
2019-04-15 20:19:20 -0700

    Bump: version to 0.0.21

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from os import path
 from setuptools import setup, find_packages
 
 NAME = "azure-functions-devops-build"
-VERSION = "0.0.20"
+VERSION = "0.0.21"
 
 REQUIRES = ["msrest",
             "vsts",
